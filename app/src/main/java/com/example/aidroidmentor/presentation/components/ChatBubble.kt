4e35cb99d2f5a1dc4f2e4a639f8fef04dfcf36ff
Jaydeep Raval <rjaydeep@joulepoint.com>
2026-08-14 10:08:45 +0530

    Add chat save feature with Room database

--- app/src/main/java/com/example/aidroidmentor/presentation/components/ChatBubble.kt
+++ app/src/main/java/com/example/aidroidmentor/presentation/components/ChatBubble.kt
@@ -1,12 +1,18 @@
+package com.example.aidroidmentor.presentation.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.outlined.BookmarkBorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import com.example.aidroidmentor.videmodels.ChatMessage
 
 @Composable
@@ -14,6 +20,10 @@ fun ChatBubble(
     message: ChatMessage,
     onSave: () -> Unit
 ) {
+
+    var isSaved by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     Column {
 
@@ -24,11 +34,22 @@ fun ChatBubble(
         Row {
 
             IconButton(
-                onClick = onSave
+                onClick = {
+                    isSaved = !isSaved
+                    onSave()
+                }
             ) {
                 Icon(
-                    imageVector = Icons.Default.Bookmark,
-                    contentDescription = "Save"
+                    imageVector = if (isSaved) {
+                        Icons.Default.Bookmark
+                    } else {
+                        Icons.Outlined.BookmarkBorder
+                    },
+                    contentDescription = if (isSaved) {
+                        "Unsave"
+                    } else {
+                        "Save"
+                    }
                 )
             }
 
